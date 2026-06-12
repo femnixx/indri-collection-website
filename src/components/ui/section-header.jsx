246fb0d85e7ece61cb328811cf5b70d51e69125a
dvns1278 <dealovasalmah12@gmail.com>
2026-06-12 23:48:47 +0700

    style: standarize margin

--- src/components/ui/section-header.jsx
+++ src/components/ui/section-header.jsx
@@ -4,7 +4,7 @@ export default function SectionHeader({ title, highlightedText, align = 'center'
   const alignClass = align === 'center' ? 'text-center' : 'text-left';
   
   return (
-    <div className={`${alignClass} mb-12`}>
+    <div className={`${alignClass} mb-6`}>
       <h2 className="text-3xl md:text-4xl font-bold text-[#10324A]">
         {title} <span className="text-[#00B2FF]">{highlightedText}</span>
       </h2>
