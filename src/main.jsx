ff1cce3b59b141fab3885ca7076682ffc6b96630
unknown <femnitality@gmail.com>
2026-07-02 22:20:46 +0700

    feat: import for react instead of next

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 // 1. Import the React-specific Analytics component
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next" 
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
