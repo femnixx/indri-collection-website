a039cba9c21ad4577ff0e335d5a4413e538fbf18
dvsalmah <dealovasalmah12@gmail.com>
2026-07-12 22:11:52 +0700

    chore: remove tracked Next.js build

--- .next/dev/static/chunks/src_app_page_tsx_1r2tbvd._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_01oj90f._.js","static/chunks/node_modules_0kzqu5c._.js"],
-    source: "dynamic"
-});
